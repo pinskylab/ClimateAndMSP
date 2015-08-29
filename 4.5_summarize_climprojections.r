c3e16656677f244abd877b2a388126f1600c7c4d
Malin Pinsky <malin.pinsky@gmail.com>
2015-08-29 01:56:24 -0400

    Climate projection summaries with SST
    for the first time!
    raw data are sitting on Amphiprion (files are 100s MBs)

--- 4.5_summarize_climprojections.r
+++ 4.5_summarize_climprojections.r
@@ -68,7 +68,7 @@ save(delta2100long2, file='data/delta2100long2.RData') # slow
 ## summarize raw delta data by time period
 ###########################################
 # uses 80GB of RAM on Amphiprion
-# load delta2100long2.RData if needed
+# load delta2100long2.RData if needed. don't reload if already in memory, since very slow
 
 delta2100long3 = melt(delta2100long2, id = c('lon', 'lat', 'depth', 'season', 'year', 'region', 'model')) # melted version for manipulation
 	dim(delta2100long3) # 309,792,340 rows
@@ -178,7 +178,6 @@ load('data/deltaSSTperiods.RData') # average SST delta by time period for each g
 	cols = colorRampPalette(colors = c('blue', 'white', 'red'))
 	pdf(width=30, height=6, file=paste('figures/climSSTproj_10deginterp.pdf', sep=''))
 	regs = sort(unique(clim2periods$region))
-		regs = regs[!grepl('Newfoundland|WCAnn', regs)]
 	seasonnames = c('winter', 'spring', 'summer', 'fall')
 	for(i in 1:length(regs)) {
 		print(i)
@@ -198,139 +197,139 @@ load('data/deltaSSTperiods.RData') # average SST delta by time period for each g
 
 # Plot biplot of historical and future temperatures by region and model (annual data)
 ## OLD CODE. WON'T WORK UNTIL UPDATED
-	data = read.csv('../../Princeton/Trawl Data/Output/goodhauls_allregions_2012-11-19.csv', row.names=1)
-	data$lon[data$lon < 0] = data$lon[data$lon < 0] + 360 # fix lons to only positive to match CMIP5 data
-	temp = data[complete.cases(data[,c('lat', 'lon', 'depth')]) & data$region %in% c('AFSC_EBS', 'AFSC_Aleutians', 'AFSC_GOA', 'WestCoast_Tri', 'SEFSC_GOMex', 'NEFSC_Spring', 'DFO_ScotianShelf', 'DFO_SoGulf', 'DFO_Newfoundland_Fall') ,c('region', 'stratum', 'lat', 'lon', 'yearsurv', 'depth', 'bottemp', 'surftemp')]
-		temp = droplevels(temp)	
-	temp = temp[temp$year<2006,] # Trim to no later than December 16, 2005 (to match historical period in CMIP5)
-	gridsize=0.25 # size of grid of the climate data, in degrees
-	temp$latgrid = floor(temp$lat/gridsize)*gridsize + gridsize/2 # round to nearest grid center
-	temp$longrid = floor(temp$lon/gridsize)*gridsize + gridsize/2
-
-	regs = sort(unique(clim$region))
-	quartz(height=9, width=9)
-	pdf(height = 9, width = 9, file=paste('Figures/nonanalog.pdf', sep=''))
-
-	for(i in 1:13){ # for each climate model
-		print(i)
-		#bmp(height = 9, width = 9, res=300, units='in', file=paste('Figures/nonanalog_', i, '_', Sys.Date(), '.bmp', sep=''))
-		par(mfrow=c(3,3), oma=c(0,0,1.5,0), mai= c(0.5, 0.5, 0.3, 0.1), mgp=c(2,1,0))
-		for(j in 1:length(regs)){
-			inds = clim$region == regs[j]
-			inds1 = temp$region == regs[j]
-			xlims = range(c(temp$bottemp[inds1], clim[[paste('bottemp.proj_', i, sep='')]][inds]), na.rm=TRUE)
-			
-			if(regs[j] == 'DFO_Newfoundland_Fall'){
-				inds2 = clim$year %in% (2061:2100)
-				plot(clim[[paste('bottemp.proj_', i, sep='')]][inds & inds2], jitter(rep(1, sum(inds&inds2))), col='red', xlab='Bottom temperature (°C)', ylab='not an axis', main=regs[j], xlim=xlims)
-				inds2 = clim$year %in% (2020:2060)
-				points(clim[[paste('bottemp.proj_', i, sep='')]][inds & inds2], jitter(rep(1, sum(inds&inds2))), col='blue')
-				points(temp$bottemp[inds1],  jitter(rep(1, sum(inds1))))				
-			} else {
-				ylims = range(c(temp$surftemp[inds1], clim[[paste('surftemp.proj_', i, sep='')]][inds]), na.rm=TRUE)
-			
-				inds2 = clim$year %in% (2061:2100)
-				plot(clim[[paste('bottemp.proj_', i, sep='')]][inds&inds2], clim[[paste('surftemp.proj_', i, sep='')]][inds&inds2], col='red', xlab='Bottom temperature (°C)', ylab='Surface temperature (°C)', main=regs[j], xlim=xlims, ylim=ylims)
-				inds2 = clim$year %in% (2020:2060)
-				points(clim[[paste('bottemp.proj_', i, sep='')]][inds&inds2], clim[[paste('surftemp.proj_', i, sep='')]][inds&inds2], col='blue')
-				points(temp$bottemp[inds1], temp$surftemp[inds1])
-			}
-		}
-		mtext(names(delta2060long)[i], outer=TRUE, side=3)
-		#dev.off()
-	}
-	
-	dev.off()
-
-
-
-# Plot maps of where nonanalog future temperatures will occur, by region and model (using climatologies) 
-# OLD CODE. WON'T WORK UNTIL UPDATED
-	regs = sort(unique(clim$region))
-	pch =16
-	cex = 0.7
-
-	pdf(height = 9, width = 9, file=paste('figures/nonanalog_maps.pdf', sep=''))
-
-	for(i in 1:13){ # for each climate model
-		par(mfrow=c(3,3), oma=c(0,0,1.5,0), mai= c(0.5, 0.5, 0.3, 0.1), mgp=c(2,1,0))
-		for(j in 1:length(regs)){
-			inds = clim$region == regs[j]
-			inds1 = temp$region == regs[j]
-			maxb = max(temp$bottemp[inds1], na.rm=TRUE)
-
-			if(regs[j] == 'DFO_Newfoundland_Fall'){
-				nonb = clim[[paste('bottemp.2100_', i, sep='')]][inds] > maxb # BT exceeds historical max
-				plot(clim$lon[inds], clim$lat[inds], col='grey', xlab='Lon', ylab='Lat', main=regs[j], pch=pch, cex=cex)
-				points(clim$lon[inds][nonb], clim$lat[inds][nonb], col='blue', pch=pch, cex=cex)
-			} else {
-				maxs = max(temp$surftemp[inds1], na.rm=TRUE)
-				nonb = ((clim[[paste('bottemp.proj_', i, sep='')]][inds] > maxb) & !(clim[[paste('surftemp.proj_', i, sep='')]][inds] > maxs)) # BT but not SST exceeds historical max
-				nons = (!(clim[[paste('bottemp.proj_', i, sep='')]][inds] > maxb) & (clim[[paste('surftemp.proj_', i, sep='')]][inds] > maxs)) # SST but not BT exceeds historical max
-				non2 = 2 == ((clim[[paste('bottemp.proj_', i, sep='')]][inds] > maxb) + (clim[[paste('surftemp.proj_', i, sep='')]][inds] > maxs)) # both of BT and SST exceeds historical max
-			
-				plot(clim$lon[inds], clim$lat[inds], col='grey', xlab='Lon', ylab='Lat', main=regs[j], pch=pch, cex=cex)
-				points(clim$lon[inds][nonb], clim$lat[inds][nonb], col='blue', pch=pch, cex=cex)
-				points(clim$lon[inds][nons], clim$lat[inds][nons], col='green', pch=pch, cex=cex)
-				points(clim$lon[inds][non2], clim$lat[inds][non2], col='red', pch=pch, cex=cex)
-			}
-		}
-		mtext(names(delta2060long)[i], outer=TRUE, side=3)
-	}
-	
-	dev.off()
-
-# Plot maps of where nonanalog future temperatures will occur by region (for BT and for SST, summarized across models)
-# OLD CODE. WON'T WORK UNTIL UPDATED
-	regs = sort(unique(clim$region))
-	pch =16
-	cex = 0.7
-	cols = c(gray(0.5), rev(heat.colors(13)))
-
-	pdf(height = 9, width = 9, file=paste('figures/nonanalog_mapsummary.pdf', sep=''))
-
-	# for BT
-	par(mfrow=c(3,3), oma=c(0,0,1.5,0), mai= c(0.5, 0.5, 0.3, 0.1), mgp=c(2,1,0))
-	for(j in 1:length(regs)){
-		inds = temp$region == regs[j]
-		maxb = max(temp$bottemp[inds], na.rm=TRUE)
-
-		inds1 = clim$region == regs[j]	
-		ct = rep(0, length(unique(paste(clim$lat[inds1], clim$lon[inds1])))) # counts how many models exceed maxb at each lat/lon
-		for(i in 1:13){ # for each model
-			modb = aggregate(list(bottemp = clim[[paste('bottemp.proj_', i, sep='')]][inds1]), by=list(lat = clim$lat[inds1], lon = clim$lon[inds1]), FUN=max, na.rm=TRUE) # find max temp at each lat/lon in the model projections
-			modb = modb[order(modb$lat, modb$lon),]
-			ct = ct + (modb$bottemp > maxb)
-		}
-			
-		plot(modb$lon, modb$lat, col=cols[ct+1], xlab='Lon', ylab='Lat', main=regs[j], pch=pch, cex=cex)
-	}
-	mtext("Bottom Temperature 2060-2100", outer=TRUE, side=3)
-	legend('bottomleft', col=cols, pch=16, legend=0:13, cex=0.9)
-
-	# for SST
-	par(mfrow=c(3,3), oma=c(0,0,1.5,0), mai= c(0.5, 0.5, 0.3, 0.1), mgp=c(2,1,0))
-	for(j in 1:length(regs)){
-		inds = temp$region == regs[j]
-		inds1 = clim$region == regs[j]
-		if(regs[j] == 'DFO_Newfoundland_Fall'){
-			plot(clim$lon[inds1], clim$lat[inds1], col='white', xlab='Lon', ylab='Lat', main=regs[j], pch=pch, cex=cex)
-		} else {
-			maxb = max(temp$surftemp[inds], na.rm=TRUE)
-	
-			ct = rep(0, length(unique(paste(clim$lat[inds1], clim$lon[inds1])))) # counts how many models exceed maxb at each lat/lon
-			for(i in 1:13){ # for each model
-				modb = aggregate(list(surftemp = clim[[paste('surftemp.proj_', i, sep='')]][inds1]), by=list(lat = clim$lat[inds1], lon = clim$lon[inds1]), FUN=max, na.rm=TRUE) # find max temp at each lat/lon in the model projections
-				modb = modb[order(modb$lat, modb$lon),]
-				ct = ct + (modb$surftemp > maxb)
-			}
-			
-			plot(modb$lon, modb$lat, col=cols[ct+1], xlab='Lon', ylab='Lat', main=regs[j], pch=pch, cex=cex)
-		}
-	}
-	mtext("Surface Temperature 2060-2100", outer=TRUE, side=3)
-	legend('bottomleft', col=cols, pch=16, legend=0:13, cex=0.9)
-
-	
-	dev.off()
-
+#	data = read.csv('../../Princeton/Trawl Data/Output/goodhauls_allregions_2012-11-19.csv', row.names=1)
+#	data$lon[data$lon < 0] = data$lon[data$lon < 0] + 360 # fix lons to only positive to match CMIP5 data
+#	temp = data[complete.cases(data[,c('lat', 'lon', 'depth')]) & data$region %in% c('AFSC_EBS', 'AFSC_Aleutians', 'AFSC_GOA', 'WestCoast_Tri', 'SEFSC_GOMex', 'NEFSC_Spring', 'DFO_ScotianShelf', 'DFO_SoGulf', 'DFO_Newfoundland_Fall') ,c('region', 'stratum', 'lat', 'lon', 'yearsurv', 'depth', 'bottemp', 'surftemp')]
+#		temp = droplevels(temp)	
+#	temp = temp[temp$year<2006,] # Trim to no later than December 16, 2005 (to match historical period in CMIP5)
+#	gridsize=0.25 # size of grid of the climate data, in degrees
+#	temp$latgrid = floor(temp$lat/gridsize)*gridsize + gridsize/2 # round to nearest grid center
+#	temp$longrid = floor(temp$lon/gridsize)*gridsize + gridsize/2
+#
+#	regs = sort(unique(clim$region))
+#	quartz(height=9, width=9)
+#	pdf(height = 9, width = 9, file=paste('Figures/nonanalog.pdf', sep=''))
+#
+#	for(i in 1:13){ # for each climate model
+#		print(i)
+#		#bmp(height = 9, width = 9, res=300, units='in', file=paste('Figures/nonanalog_', i, '_', Sys.Date(), '.bmp', sep=''))
+#		par(mfrow=c(3,3), oma=c(0,0,1.5,0), mai= c(0.5, 0.5, 0.3, 0.1), mgp=c(2,1,0))
+#		for(j in 1:length(regs)){
+#			inds = clim$region == regs[j]
+#			inds1 = temp$region == regs[j]
+#			xlims = range(c(temp$bottemp[inds1], clim[[paste('bottemp.proj_', i, sep='')]][inds]), na.rm=TRUE)
+#			
+#			if(regs[j] == 'DFO_Newfoundland_Fall'){
+#				inds2 = clim$year %in% (2061:2100)
+#				plot(clim[[paste('bottemp.proj_', i, sep='')]][inds & inds2], jitter(rep(1, sum(inds&inds2))), col='red', xlab='Bottom temperature (°C)', ylab='not an axis', main=regs[j], xlim=xlims)
+#				inds2 = clim$year %in% (2020:2060)
+#				points(clim[[paste('bottemp.proj_', i, sep='')]][inds & inds2], jitter(rep(1, sum(inds&inds2))), col='blue')
+#				points(temp$bottemp[inds1],  jitter(rep(1, sum(inds1))))				
+#			} else {
+#				ylims = range(c(temp$surftemp[inds1], clim[[paste('surftemp.proj_', i, sep='')]][inds]), na.rm=TRUE)
+#			
+#				inds2 = clim$year %in% (2061:2100)
+#				plot(clim[[paste('bottemp.proj_', i, sep='')]][inds&inds2], clim[[paste('surftemp.proj_', i, sep='')]][inds&inds2], col='red', xlab='Bottom temperature (°C)', ylab='Surface temperature (°C)', main=regs[j], xlim=xlims, ylim=ylims)
+#				inds2 = clim$year %in% (2020:2060)
+#				points(clim[[paste('bottemp.proj_', i, sep='')]][inds&inds2], clim[[paste('surftemp.proj_', i, sep='')]][inds&inds2], col='blue')
+#				points(temp$bottemp[inds1], temp$surftemp[inds1])
+#			}
+#		}
+#		mtext(names(delta2060long)[i], outer=TRUE, side=3)
+#		#dev.off()
+#	}
+#	
+#	dev.off()
+#
+#
+#
+## Plot maps of where nonanalog future temperatures will occur, by region and model (using climatologies) 
+## OLD CODE. WON'T WORK UNTIL UPDATED
+#	regs = sort(unique(clim$region))
+#	pch =16
+#	cex = 0.7
+#
+#	pdf(height = 9, width = 9, file=paste('figures/nonanalog_maps.pdf', sep=''))
+#
+#	for(i in 1:13){ # for each climate model
+#		par(mfrow=c(3,3), oma=c(0,0,1.5,0), mai= c(0.5, 0.5, 0.3, 0.1), mgp=c(2,1,0))
+#		for(j in 1:length(regs)){
+#			inds = clim$region == regs[j]
+#			inds1 = temp$region == regs[j]
+#			maxb = max(temp$bottemp[inds1], na.rm=TRUE)
+#
+#			if(regs[j] == 'DFO_Newfoundland_Fall'){
+#				nonb = clim[[paste('bottemp.2100_', i, sep='')]][inds] > maxb # BT exceeds historical max
+#				plot(clim$lon[inds], clim$lat[inds], col='grey', xlab='Lon', ylab='Lat', main=regs[j], pch=pch, cex=cex)
+#				points(clim$lon[inds][nonb], clim$lat[inds][nonb], col='blue', pch=pch, cex=cex)
+#			} else {
+#				maxs = max(temp$surftemp[inds1], na.rm=TRUE)
+#				nonb = ((clim[[paste('bottemp.proj_', i, sep='')]][inds] > maxb) & !(clim[[paste('surftemp.proj_', i, sep='')]][inds] > maxs)) # BT but not SST exceeds historical max
+#				nons = (!(clim[[paste('bottemp.proj_', i, sep='')]][inds] > maxb) & (clim[[paste('surftemp.proj_', i, sep='')]][inds] > maxs)) # SST but not BT exceeds historical max
+#				non2 = 2 == ((clim[[paste('bottemp.proj_', i, sep='')]][inds] > maxb) + (clim[[paste('surftemp.proj_', i, sep='')]][inds] > maxs)) # both of BT and SST exceeds historical max
+#			
+#				plot(clim$lon[inds], clim$lat[inds], col='grey', xlab='Lon', ylab='Lat', main=regs[j], pch=pch, cex=cex)
+#				points(clim$lon[inds][nonb], clim$lat[inds][nonb], col='blue', pch=pch, cex=cex)
+#				points(clim$lon[inds][nons], clim$lat[inds][nons], col='green', pch=pch, cex=cex)
+#				points(clim$lon[inds][non2], clim$lat[inds][non2], col='red', pch=pch, cex=cex)
+#			}
+#		}
+#		mtext(names(delta2060long)[i], outer=TRUE, side=3)
+#	}
+#	
+#	dev.off()
+#
+## Plot maps of where nonanalog future temperatures will occur by region (for BT and for SST, summarized across models)
+## OLD CODE. WON'T WORK UNTIL UPDATED
+#	regs = sort(unique(clim$region))
+#	pch =16
+#	cex = 0.7
+#	cols = c(gray(0.5), rev(heat.colors(13)))
+#
+#	pdf(height = 9, width = 9, file=paste('figures/nonanalog_mapsummary.pdf', sep=''))
+#
+#	# for BT
+#	par(mfrow=c(3,3), oma=c(0,0,1.5,0), mai= c(0.5, 0.5, 0.3, 0.1), mgp=c(2,1,0))
+#	for(j in 1:length(regs)){
+#		inds = temp$region == regs[j]
+#		maxb = max(temp$bottemp[inds], na.rm=TRUE)
+#
+#		inds1 = clim$region == regs[j]	
+#		ct = rep(0, length(unique(paste(clim$lat[inds1], clim$lon[inds1])))) # counts how many models exceed maxb at each lat/lon
+#		for(i in 1:13){ # for each model
+#			modb = aggregate(list(bottemp = clim[[paste('bottemp.proj_', i, sep='')]][inds1]), by=list(lat = clim$lat[inds1], lon = clim$lon[inds1]), FUN=max, na.rm=TRUE) # find max temp at each lat/lon in the model projections
+#			modb = modb[order(modb$lat, modb$lon),]
+#			ct = ct + (modb$bottemp > maxb)
+#		}
+#			
+#		plot(modb$lon, modb$lat, col=cols[ct+1], xlab='Lon', ylab='Lat', main=regs[j], pch=pch, cex=cex)
+#	}
+#	mtext("Bottom Temperature 2060-2100", outer=TRUE, side=3)
+#	legend('bottomleft', col=cols, pch=16, legend=0:13, cex=0.9)
+#
+#	# for SST
+#	par(mfrow=c(3,3), oma=c(0,0,1.5,0), mai= c(0.5, 0.5, 0.3, 0.1), mgp=c(2,1,0))
+#	for(j in 1:length(regs)){
+#		inds = temp$region == regs[j]
+#		inds1 = clim$region == regs[j]
+#		if(regs[j] == 'DFO_Newfoundland_Fall'){
+#			plot(clim$lon[inds1], clim$lat[inds1], col='white', xlab='Lon', ylab='Lat', main=regs[j], pch=pch, cex=cex)
+#		} else {
+#			maxb = max(temp$surftemp[inds], na.rm=TRUE)
+#	
+#			ct = rep(0, length(unique(paste(clim$lat[inds1], clim$lon[inds1])))) # counts how many models exceed maxb at each lat/lon
+#			for(i in 1:13){ # for each model
+#				modb = aggregate(list(surftemp = clim[[paste('surftemp.proj_', i, sep='')]][inds1]), by=list(lat = clim$lat[inds1], lon = clim$lon[inds1]), FUN=max, na.rm=TRUE) # find max temp at each lat/lon in the model projections
+#				modb = modb[order(modb$lat, modb$lon),]
+#				ct = ct + (modb$surftemp > maxb)
+#			}
+#			
+#			plot(modb$lon, modb$lat, col=cols[ct+1], xlab='Lon', ylab='Lat', main=regs[j], pch=pch, cex=cex)
+#		}
+#	}
+#	mtext("Surface Temperature 2060-2100", outer=TRUE, side=3)
+#	legend('bottomleft', col=cols, pch=16, legend=0:13, cex=0.9)
+#
+#	
+#	dev.off()
+#
