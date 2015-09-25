e461536902d3241ae1f4cffd8919543773cbc39c
Malin Pinsky <malin.pinsky@gmail.com>
2015-09-25 15:55:15 -0400

    Removed seasons from climatology
    Found an error in the way I was averaging across decades, which led me
    to realize that we don’t have enough data to create climatologies by
    season within each year. Output a new climGrid.csv as well as new
    climatology figures.

--- 2_create_climatologies.r
+++ 2_create_climatologies.r
@@ -36,65 +36,77 @@ load("data/trawl_allregionsforprojections_wSST_2015-06-02.RData") # loads dat
 		yrs = sort(unique(temp$year[inds]))
 		decs = seq(min(yrs), max(yrs), by=10) + 5 # center of each decade
 		temp$decade[inds] = floor((temp$yearsurv[inds] - min(decs)+5)/10)*10+min(decs)
-	}
+		decdf <- data.frame(decindex=1:length(unique(temp$decade[inds])), decade=sort(unique(temp$decade[inds])))
+			row.names(decdf) <- decdf$decade
+			
+		if(nrow(decdf)>1 & (max(yrs) - max(decdf$decade) < 0)){ # if the survey ends less than half-way through the last decade, add its survey points to the previous decade (if more than 1 decade)
+			decdf$decindex[nrow(decdf)] = decdf$decindex[nrow(decdf)-1]
+		}
+		temp$decindex[inds] <- decdf[as.character(temp$decade[inds]), 'decindex'] # index by rownames
 
-	# Label by season
-	temp$season = NA
-	inds = temp$month <= 3
-	temp$season[inds] = 1
-	inds = temp$month > 3 & temp$month<=6
-	temp$season[inds] = 2
-	inds = temp$month > 6 & temp$month<=9
-	temp$season[inds] = 3
-	inds = temp$month > 9
-	temp$season[inds] = 4
+		# look at the choices made
+		print(regs[i])
+		print(range(yrs))
+		print(decdf)
+	}
+	
+		table(temp$region, temp$decindex)
+
+	# simplistic averaging by month within regions to look at cold vs. warm seasons
+#	tempsbyreg <- aggregate(list(bottemp=temp$bottemp, surftemp=temp$surftemp), by=list(region=temp$region, month=temp$month), FUN=mean, na.rm=TRUE)
+#		require(lattice)
+#		xyplot(bottemp~month|region, data=tempsbyreg, type=c('o', 'g'), scales=list(x=list(relation='same'), y=list(relation='free')))
+#
 	
 # Average by grid cell and by decade within seasons. lat, lon are now grid centers
-	climdec = aggregate(list(bottemp.clim = temp$bottemp, surftemp.clim = temp$surftemp, depth=temp$depth), by=list(lat = temp$latgrid, lon = temp$longrid, decade = temp$decade, region=temp$region, season=temp$season), FUN=mean, na.rm=TRUE)
-		dim(climdec) # 20,228
+	climdec = aggregate(list(bottemp.clim = temp$bottemp, surftemp.clim = temp$surftemp, depth=temp$depth), by=list(lat = temp$latgrid, lon = temp$longrid, decade = temp$decindex, region=temp$region), FUN=mean, na.rm=TRUE)
+		dim(climdec) # 13,130
 		names(climdec)
+		
+# Make a list of all grid cells in all regions in all decades in all season
+	full <- data.frame(lat=numeric(0), lon=numeric(0), decade=numeric(0), region=character(0))
+	regs <- sort(unique(climdec$region))
+	for(i in 1:length(regs)){
+		inds <- climdec$region == regs[i]
+		decs <- data.frame(decade = sort(unique(climdec$decade[inds])))
+		inds2 <- climdec$region == regs[i] & !duplicated(climdec[,c('lat', 'lon', 'region')]) # unique lat/lon
+		newregdf <- merge(climdec[inds2,c('lat', 'lon', 'region')], decs)
+		full <- rbind(full, newregdf)
+	}
+	dim(full) # 15,058
+	
+# Outer join to add NAs for temperatures not measured
+	climdec2 <- merge(climdec, full, all=TRUE)
+		dim(climdec2) # 15,058
 
 # Average across decades (done as a second step to avoid one decade with lots of data swamping the average)
-	clim = aggregate(list(bottemp.clim = climdec$bottemp.clim, surftemp.clim = climdec$surftemp.clim, depth=climdec$depth), by=list(lat = climdec$lat, lon = climdec$lon,  region=climdec$region, season=climdec$season), FUN=mean) # don't remove NAs because it would mean dropping a decade
-		dim(clim) # 9988
+	clim = aggregate(list(bottemp.clim = climdec2$bottemp.clim, surftemp.clim = climdec2$surftemp.clim), by=list(lat = climdec2$lat, lon = climdec2$lon,  region=climdec2$region), FUN=mean) # don't remove NAs because it would mean dropping a decade
+		dim(clim) # 6,535
 		names(clim)
+
+	climdepth = aggregate(list(depth = climdec2$depth), by=list(lat = climdec2$lat, lon = climdec2$lon,  region=climdec2$region), FUN=mean, na.rm=TRUE) # remove NAs since not worried about decadal variability in depth
+		dim(climdepth) # 6,535
+		summary(climdepth)
+	
+	clim = merge(clim, climdepth) # merge in depth data
+		dim(clim)
 	
 		# plot each region in a separate figure. color axis is relative within each region
 		require(lattice)
 		cols = colorRampPalette(colors = c('blue', 'white', 'red'))
-		pdf(width=30, height=12, file=paste('figures/climBT_grid.pdf', sep=''))
-		levelplot(bottemp.clim ~ lon*lat|region*season, scales = list(x='free', y='free'), data=clim, at = seq(0,1, length.out=40), col.regions=cols, panel=function(...,z, subscripts, at){
+		pdf(width=10, height=9, file=paste('figures/climBT_grid.pdf', sep=''))
+		levelplot(bottemp.clim ~ lon*lat|region, scales = list(x='free', y='free'), data=clim, at = seq(0,1, length.out=40), col.regions=cols, panel=function(...,z, subscripts, at){
 				panel.fill(col='light grey')
 				panel.levelplot(..., z=z, subscripts=subscripts, at=seq(min(z[subscripts], na.rm=TRUE), max(z[subscripts], na.rm=TRUE), length.out=20))})
 
 		dev.off()
 
-		pdf(width=30, height=12, file=paste('figures/climSST_grid.pdf', sep=''))
-		levelplot(surftemp.clim ~ lon*lat|region*season, scales = list(x='free', y='free'), data=clim, at = seq(0,1, length.out=40), col.regions=cols, panel=function(...,z, subscripts, at){
+		pdf(width=10, height=9, file=paste('figures/climSST_grid.pdf', sep=''))
+		levelplot(surftemp.clim ~ lon*lat|region, scales = list(x='free', y='free'), data=clim, at = seq(0,1, length.out=40), col.regions=cols, panel=function(...,z, subscripts, at){
 				panel.fill(col='light grey')
 				panel.levelplot(..., z=z, subscripts=subscripts, at=seq(min(z[subscripts], na.rm=TRUE), max(z[subscripts], na.rm=TRUE), length.out=20))})
 
 		dev.off()
-
-# Choose the seasons within each region to interpolate
-table(clim$region, clim$season)
-
-	# keep region/seasons that have enough data for decent interpolation
-keep = c('AFSC_Aleutians 2', 'AFSC_Aleutians 3', 'AFSC_EBS 2', 'AFSC_EBS 3', 'AFSC_GOA 3', 'AFSC_WCTri 3', 'DFO_NewfoundlandFall 4', 'DFO_NewfoundlandSpring 2', 'DFO_ScotianShelf 1', 'DFO_ScotianShelf 3', 'DFO_ScotianShelf 4', 'DFO_SoGulf 3', 'NEFSC_NEUSFall 4', 'NEFSC_NEUSSpring 1', 'NEFSC_NEUSSpring 2', 'NWFSC_WCAnn 3', 'SEFSC_GOMex 2', 'SEFSC_GOMex 3')
-
-clim = clim[paste(clim$region, clim$season) %in% keep,] # trim down to these region/season combinations
-	dim(clim) # 8046
-climold = clim # save a copy
-
-	# Scotian Shelf winter extends far south. Trim this off by only keep gridcells present in at least two seasons
-	sskeepgrids = clim[clim$region == 'DFO_ScotianShelf' & !duplicated(clim[,c('region', 'lat', 'lon', 'season')]), c('region', 'lat', 'lon', 'season')] # list of Scotian Shelf gridcells and the season in which they appear
-	sskeepgrids2 = reshape(sskeepgrids, direction='wide', timevar='season', v.names='season', idvar = c('region', 'lat', 'lon')) # reshape to wide to easily see which grids cells appear in which seasons
-	sskeepgrids3 = sskeepgrids2[rowSums(!is.na(sskeepgrids2[,c('season.1', 'season.3', 'season.4')]))>1,] # only keep those grids present in at least 2 seasons (e.g., not only winter)
-
-	clim = clim[clim$region != 'DFO_ScotianShelf' | (paste(clim$lat, clim$lon) %in% paste(sskeepgrids3$lat, sskeepgrids3$lon)),] # trim out the Scotian Shelf grids that didn't make the cut
-	
-	nrow(climold)-nrow(clim) # dropped 165 rows
-	nrow(sskeepgrids2)-nrow(sskeepgrids3) # also dropped 165 rows from Scotian Shelf: perfect (to make sure I didn't drop anything extra)
 
 
 # Interpolate temperatures
@@ -111,68 +123,50 @@ climold = clim # save a copy
 		return(d = R * c) # Distance in km
 	}
 
-	# make sure all grid cells appear in each season
-	regs = sort(unique(clim$region))
-	for(i in 1:length(regs)){
-		grids = clim[!duplicated(clim[,c('lat', 'lon')]) & clim$region == regs[i], c('lat', 'lon', 'depth')] # will arbitrarily pull the first depth value for each grid cell
-		seasons = sort(unique(clim$season[clim$region == regs[i]]))
-		for(j in seasons){ # for each season
-			thesegrids = clim[!duplicated(clim[,c('lat', 'lon', 'season')]) & clim$region == regs[i] & clim$season == j, c('lat', 'lon')]
-			newrows = grids[!(paste(grids$lat, grids$lon) %in% paste(thesegrids$lat, thesegrids$lon)),]
-			if(nrow(newrows)>0){
-				newrows$region = regs[i]
-				newrows$season = j
-				newrows$bottemp.clim = NA
-				newrows$surftemp.clim = NA
-				clim = rbind(clim, newrows)
-			}
-		}
-	}
-	dim(clim) # 8633
-
+	sum(is.na(clim$bottemp.clim)) # = 1371: some missing values
 	inds = which(is.na(clim$bottemp.clim)) # points to fill for BT
 	clim$bottemp.clim.int = clim$bottemp.clim # interpolated BT
 	for(i in 1:length(inds)){
 		lt = clim$lat[inds[i]]
 		ln = clim$lon[inds[i]]
-		nn = clim$lat %in% c(lt, lt-gridsize, lt+gridsize, lt-gridsize*2, lt+gridsize*2, lt-gridsize*3, lt+gridsize*3) & clim$lon %in% c(ln, ln-gridsize, ln+gridsize, ln-gridsize*2, ln+gridsize*2, ln-gridsize*3, ln+gridsize*3) & clim$region == unique(clim$region[inds[i]]) & clim$season == unique(clim$season[inds[i]]) # nearest neighbors from same region and season (including the focal grid cell)
+		nn = clim$lat %in% c(lt, lt-gridsize, lt+gridsize, lt-gridsize*2, lt+gridsize*2, lt-gridsize*3, lt+gridsize*3) & clim$lon %in% c(ln, ln-gridsize, ln+gridsize, ln-gridsize*2, ln+gridsize*2, ln-gridsize*3, ln+gridsize*3) & clim$region == unique(clim$region[inds[i]]) # nearest neighbors from same region (including the focal grid cell)
 		dist = pmax(1, gcd.hf(ln, lt, clim$lon[nn], clim$lat[nn])) # distances in km, but set 1km as a floor (so no zeros)
 		clim$bottemp.clim.int[inds[i]] = weighted.mean(x=clim$bottemp.clim[nn], w=1/dist, na.rm=TRUE) # weight values from 3 grid cells in every direction by 1/distance
 	}	
-		sum(is.na(clim$bottemp.clim.int)) # = 251: some missing values
-		unique(clim$region[is.na(clim$bottemp.clim.int)]) # EBS, NEUSSpring, GOMex
+		sum(is.na(clim$bottemp.clim.int)) # = 379: some missing values
+		unique(clim$region[is.na(clim$bottemp.clim.int)]) # WCTri, ScotianShelf, NEUSSpring and Fall, GOMex
 		table(clim$region, is.na(clim$bottemp.clim.int))
 
+	sum(is.na(clim$surftemp.clim)) # = 1173 missing SST values
 	inds = which(is.na(clim$surftemp.clim)) # points to fill for SST
 	clim$surftemp.clim.int = clim$surftemp.clim # interpolated ST
 	for(i in 1:length(inds)){
 		lt = clim$lat[inds[i]]
 		ln = clim$lon[inds[i]]
-		nn = clim$lat %in% c(lt, lt-gridsize, lt+gridsize, lt-gridsize*2, lt+gridsize*2, lt-gridsize*3, lt+gridsize*3) & clim$lon %in% c(ln, ln-gridsize, ln+gridsize, ln-gridsize*2, ln+gridsize*2, ln-gridsize*3, ln+gridsize*3) & clim$region == unique(clim$region[inds[i]])  & clim$season == unique(clim$season[inds[i]]) # nearest neighbors (including the focal grid cell)
+		nn = clim$lat %in% c(lt, lt-gridsize, lt+gridsize, lt-gridsize*2, lt+gridsize*2, lt-gridsize*3, lt+gridsize*3) & clim$lon %in% c(ln, ln-gridsize, ln+gridsize, ln-gridsize*2, ln+gridsize*2, ln-gridsize*3, ln+gridsize*3) & clim$region == unique(clim$region[inds[i]])# nearest neighbors (including the focal grid cell)
 		dist = pmax(1,gcd.hf(ln, lt, clim$lon[nn], clim$lat[nn])) # distances in km (>=1)
 		clim$surftemp.clim.int[inds[i]] = weighted.mean(x=clim$surftemp.clim[nn], w=1/dist, na.rm=TRUE)
 	}	
-		sum(is.na(clim$surftemp.clim.int)) # = 247 missing SST values
-		unique(clim$region[is.na(clim$surftemp.clim.int)]) # EBS, NEUSSpring, GOMex.
+		sum(is.na(clim$surftemp.clim.int)) # = 378 missing SST values
+		unique(clim$region[is.na(clim$surftemp.clim.int)]) # WCTri, ScotianShelf, NEUSSpring and Fall, GOMex
 		table(clim$region, is.na(clim$surftemp.clim.int))
 
-
 	inds = which(is.na(clim$depth)) # points to fill for depth (0)
-		sum(inds)
+		length(inds)
 
 	# plot each region in a separate figure (interpolated temp)
 		#BT
 	require(lattice)
 	cols = colorRampPalette(colors = c('blue', 'white', 'red'))
-	pdf(width=30, height=12, file=paste('figures/climBT_grid_interp.pdf', sep=''))
-	levelplot(bottemp.clim.int ~ lon*lat|region*season, scales = list(x='free', y='free'), data=clim, at = seq(0,1, length.out=40), col.regions = cols, panel=function(...,z, subscripts, at){
+	pdf(width=10, height=9, file=paste('figures/climBT_grid_interp.pdf', sep=''))
+	levelplot(bottemp.clim.int ~ lon*lat|region, scales = list(x='free', y='free'), data=clim, at = seq(0,1, length.out=40), col.regions = cols, panel=function(...,z, subscripts, at){
 			panel.fill(col='light grey')
 			panel.levelplot(..., z=z, subscripts=subscripts, at=seq(min(z[subscripts], na.rm=TRUE), max(z[subscripts], na.rm=TRUE), length.out=20))})
 	dev.off()
 
 		#SST
-	pdf(width=30, height=12, file=paste('figures/climSST_grid_interp.pdf', sep=''))
-	levelplot(surftemp.clim.int ~ lon*lat|region*season, scales = list(x='free', y='free'), data=clim, at = seq(0,1, length.out=40), col.regions = cols, panel=function(...,z, subscripts, at){
+	pdf(width=10, height=9, file=paste('figures/climSST_grid_interp.pdf', sep=''))
+	levelplot(surftemp.clim.int ~ lon*lat|region, scales = list(x='free', y='free'), data=clim, at = seq(0,1, length.out=40), col.regions = cols, panel=function(...,z, subscripts, at){
 			panel.fill(col='light grey')
 			panel.levelplot(..., z=z, subscripts=subscripts, at=seq(min(z[subscripts], na.rm=TRUE), max(z[subscripts], na.rm=TRUE), length.out=20))})
 
