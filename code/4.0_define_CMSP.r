2a9d928f82288bbb0c2e2ac3b63f4f6066c68904
Malin Pinsky <malin.pinsky@gmail.com>
2019-10-25 13:18:21 +0200

    Define fishery species for each region

--- code/4.0_define_CMSP.r
+++ code/4.0_define_CMSP.r
@@ -10,7 +10,7 @@ require(sf)
 # read in data
 grid <- readRDS('temp/SPsf2.rds') # analysis grid
 eez <- st_read('dataDL/marineregions/World_EEZ_v10_20180221/eez_v10.shp')
-# coast <- st_read('dataDL/natcap/Marine/Land/global_polyline.shp') # useful for checking success by making maps
+coast <- st_read('dataDL/natcap/Marine/Land/global_polyline.shp') # useful for checking success by making maps
 
 # get ready to define regions
 indsCAN <- st_intersects(eez[eez$Pol_type == '200NM' & eez$Territory1 == 'Canada',], grid, sparse = FALSE) # slow...
@@ -57,117 +57,81 @@ with(grid[grid$region == 'seus', ], plot(lon, lat, cex=0.1))
 with(grid[grid$region == 'gomex', ], plot(lon, lat, cex=0.1))
 with(grid[grid$region == 'wc', ], plot(lon, lat, cex=0.1))
 with(grid[grid$region == 'bc', ], plot(lon, lat, cex=0.1))
-with(grid[grid$region == 'goa', ], plot(lon, lat, cex=0.1)) # odd chunk out of SW corner?
+with(grid[grid$region == 'goa', ], plot(lon, lat, cex=0.1)) # odd chunk out of SW corner? but nearly all is land
+    plot(st_geometry(coast), add=TRUE)
 with(grid[grid$region == 'ebs', ], plot(lon, lat, cex=0.1))
 
 # write out
 saveRDS(grid, 'output/region_grid.rds')
 
+
 ###########################################################
 # Determine which species are of fishery importance
-# from Sea Around Us
+# from Sea Around Us fishery catch data
 ###########################################################
+require(data.table)
+
 ## get list of projected species
-load(paste('data/presmap_', runtype, projtype, '_rcp', rcp, '.RData', sep='')) # loads biomassavemap data.frame
-head(presmap)
-spps <- sort(unique(as.character(presmap$sppocean)))
-sppstrim <- gsub('_Atl|_Pac', '', spps)
+wdpa_by_spp_projnow <- readRDS('temp/wdpa_by_spp_projnow.rds') # from 1.2_evalWDPA.r. A convenient summary of species.
+projspps <- wdpa_by_spp_projnow[, sort(unique(as.character(spp)))]
 
 # get tables of landings by LME from Sea Around Us
-ebs <- read.csv('cmsp_data/SAU/SAU LME 1 v1-40.csv')
-al <- read.csv('cmsp_data/SAU/SAU LME 65 v1-40.csv')
-goa <- read.csv('cmsp_data/SAU/SAU LME 2 v1-40.csv')
-wcann <- read.csv('cmsp_data/SAU/SAU EEZ 848 v1-40.csv')
-wctri <- wcann # west coast annual and trienniel: same LME
-gmex <- read.csv('cmsp_data/SAU/SAU LME 5 v1-40.csv')
-neuss <- read.csv('cmsp_data/SAU/SAU LME 7 v1-40.csv')
-neusf <- neuss # NEUS spring and fall: same LME
-scot <- read.csv('cmsp_data/SAU/SAU LME 8 v1-40.csv')
-sgulf <- scot # southern Gulf of St. Lawrence, same LME as Scotian Shelf 
-newff <- read.csv('cmsp_data/SAU/SAU LME 9 v1-40.csv')
-newfs <- newff # newfoundland spring and fall: same LME
+ebs <- fread('dataDL/sau/SAU LME 1 v47-1.csv')
+ai <- fread('dataDL/sau/SAU LME 65 v47-1.csv') # will combine with ebs
+goabc <- fread('dataDL/sau/SAU LME 2 v47-1.csv') # shared with goa and bc
+wc <- fread('dataDL/sau/SAU LME 3 v47-1.csv')
+gmex <- fread('dataDL/sau/SAU LME 5 v47-1.csv')
+seus <- fread('dataDL/sau/SAU LME 6 v47-1.csv')
+neus <- fread('dataDL/sau/SAU LME 7 v47-1.csv')
+maritime <- fread('dataDL/sau/SAU LME 8 v47-1.csv')
+newf <- fread('dataDL/sau/SAU LME 9 v47-1.csv')
 
 # add region
-ebs$region <- 'AFSC_EBS'
-al$region <- 'AFSC_Aleutians'
-goa$region <- 'AFSC_GOA'
-wcann$region <- 'NWFSC_WCAnn'
-wctri$region <- 'AFSC_WCTri'
-gmex$region <- 'SEFSC_GOMex'
-neuss$region <- 'NEFSC_NEUSSpring'
-neusf$region <- 'NEFSC_NEUSFall'
-scot$region <- 'DFO_ScotianShelf'
-sgulf$region <- 'DFO_SoGulf'
-newff$region <- 'DFO_NewfoundlandFall'
-newfs$region <- 'DFO_NewfoundlandSpring'
+ebs$region <- 'ebs'
+ai$region <- 'ebs'
+goabc$region <- 'goa_bc'
+wc$region <- 'wc'
+gmex$region <- 'gmex'
+seus$region <- 'seus'
+neus$region <- 'neus'
+maritime$region <- 'maritime'
+newf$region <- 'newf'
 
-# combine into a list
-sau <- list(ebs=ebs, al=al, goa=goa, wcann=wcann, wctri=wctri, gmex=gmex, neuss=neuss, neusf=neusf, scot=scot, sgulf=sgulf, newff=newff, newfs=newfs)
-
-# aggregate recent reported landings by species
-sppston <- sau
-for(i in 1:length(sau)){
-	sppston[[i]] <- with(sau[[i]][sau[[i]]$reporting_status=='Reported' & sau[[i]]$catch_type=='Landings' & sau[[i]]$year>=1990,], aggregate(list(tonnes = tonnes), by=list(region=region, scientific_name = scientific_name, common_name=common_name), FUN=sum))
-}
+# combine and aggregate recent reported commercial landings by species
+sau <- rbind(ebs, ai, goabc, wc, gmex, seus, neus, maritime, newf)
+sppston <- sau[year >= 1995 & fishing_sector == 'Industrial' & catch_type == 'Landings' & reporting_status == 'Reported', .(tonnes = sum(tonnes)), by = c('region', 'scientific_name')]
 
 # turn SAU names to lower case to match projections
-for(i in 1:length(sppston)){
-	sppston[[i]]$scientific_name <- tolower(sppston[[i]]$scientific_name)
-}
+sppston[, scientific_name := tolower(scientific_name)]
 
 # set up vector hold names that match projections
-for(i in 1:length(sppston)){
-	sppston[[i]]$projname <- NA
-}
-
+sppston[, projname := as.character(NA)]
 
 # match names from SAU to names from projections
 options(warn=1)
-for(i in 1:length(sppston)){
-	for(j in 1:length(sppston[[i]]$scientific_name)){
-		ind <- agrep(sppston[[i]]$scientific_name[j], sppstrim) # find index into spps and sppstrim
-		if(length(ind)==1) sppston[[i]]$projname[j] <- spps[ind] # only enter if exactly one match
-	}
+for(j in 1:nrow(sppston)){
+	ind <- agrep(sppston[j, scientific_name], projspps) # find index into spps
+	if(length(ind)==1) sppston[j, projname := projspps[ind]] # only enter if exactly one match
 }
-
 
 # order by decreasing landings
-for(i in 1:length(sppston)){
-	sppston[[i]] <- sppston[[i]][order(sppston[[i]]$tonnes, decreasing=TRUE),]
-}
+setkey(sppston, region, tonnes)
 
-# manually fix a few that agrep missed or filled in mistakenly
-for(i in 1:length(sppston)){
-	sppston[[i]]$projname[sppston[[i]]$scientific_name == 'clupea pallasii pallasii' & sppston[[i]]$region %in% atlregs] <- 'clupea pallasii_Atl'
-	sppston[[i]]$projname[sppston[[i]]$scientific_name == 'clupea pallasii pallasii' & sppston[[i]]$region %in% pacregs] <- 'clupea pallasii_Pac'
-	sppston[[i]]$projname[sppston[[i]]$scientific_name == 'sebastes'] <- NA
-	sppston[[i]]$projname[sppston[[i]]$scientific_name == 'doryteuthis pealeii' & sppston[[i]]$region %in% atlregs] <- 'loligo pealeii_Atl'
-	sppston[[i]]$projname[sppston[[i]]$scientific_name == 'doryteuthis pealeii' & sppston[[i]]$region %in% pacregs] <- 'loligo pealeii_Pac'
-}
+# manually fix any that agrep missed or filled in mistakenly (checked by eye)
+sppston[scientific_name == 'clupea pallasii pallasii', projname := 'clupea pallasii']
 
-# top 10 that are in projections for each region
-sppston[[1]][1:(which(cumsum(!is.na(sppston[[1]]$projname))==10)[1]),]
-sppston[[2]][1:(which(cumsum(!is.na(sppston[[2]]$projname))==10)[1]),]
-sppston[[3]][1:(which(cumsum(!is.na(sppston[[3]]$projname))==10)[1]),]
-sppston[[4]][1:(which(cumsum(!is.na(sppston[[4]]$projname))==10)[1]),]
-sppston[[5]][1:(which(cumsum(!is.na(sppston[[5]]$projname))==10)[1]),]
-sppston[[6]][1:(which(cumsum(!is.na(sppston[[6]]$projname))==10)[1]),]
-sppston[[7]][1:(which(cumsum(!is.na(sppston[[7]]$projname))==10)[1]),]
-sppston[[8]][1:(which(cumsum(!is.na(sppston[[8]]$projname))==10)[1]),]
-sppston[[9]][1:(which(cumsum(!is.na(sppston[[9]]$projname))==10)[1]),]
-sppston[[10]][1:(which(cumsum(!is.na(sppston[[10]]$projname))==10)[1]),]
-sppston[[11]][1:(which(cumsum(!is.na(sppston[[11]]$projname))==10)[1]),]
-sppston[[12]][1:(which(cumsum(!is.na(sppston[[12]]$projname))==10)[1]),]
 
-# combine into table to write out
-temp <- sppston[[1]][1:(which(cumsum(!is.na(sppston[[1]]$projname))==10)[1]),]
-temp <- temp[!is.na(temp$projname),]
-out <- temp
-for(i in 2:length(sppston)){
-	temp <- sppston[[i]][1:(which(cumsum(!is.na(sppston[[i]]$projname))==10)[1]),]
-	temp <- temp[!is.na(temp$projname),]
-	out <- rbind(out,temp)
-}
+# rank the remaining species and remove those that aren't in the projections
+sppstontrim <- sppston[!is.na(projname), .(sau_name = scientific_name, projname = projname, tonnes = tonnes, rank = rank(tonnes)), by = 'region']
+
+
+# trim to top 10 that are in projections for each region
+sppstontrim[, maxrank := max(rank), by='region']
+sppstontrim <- sppstontrim[(maxrank - rank) < 10, ]
+sppstontrim[ , c('rank', 'maxrank') := NULL] # remove unneeded columns
+
+# check that it worked
+sppstontrim[, .N, by='region']
 
 # write out
-write.csv(out, file='cmsp_data/fishery_spps.csv')
+write.csv(sppstontrim, file='output/fishery_spps.csv')
