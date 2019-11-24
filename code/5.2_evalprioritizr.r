4a527e9d75bd4cb26532fcfd42df1404d9621a5a
Malin Pinsky <malin.pinsky@gmail.com>
2019-11-24 21:46:11 +0100

    Evaluate new prioritizr runs against species projections. Split calculations separate from plotting.

--- code/5.2_evalprioritizr.r
+++ code/5.2_evalprioritizr.r
@@ -1,4 +1,5 @@
-# Evaluate the prioritzr solutions
+# Evaluate the prioritzr solutions against the range of species projections
+# Save the results: whether or not a plan meets a goal in a given model/rcp/time
 
 
 
@@ -36,10 +37,10 @@ runname2out <- '2per_all'
 ######################
 ## Helper functions
 ######################
-require(RColorBrewer)
+#require(RColorBrewer)
 require(data.table)
-require(maps)
-require(ggplot2)
+#require(maps)
+#require(ggplot2)
 
 lu <- function(x) length(unique(x))
 
@@ -65,27 +66,6 @@ if(!(length(rcps) %in% c(1,2))){
 }
 for (i in 1:length(rcps)){
     print(paste0('rcp', rcps[i]))
-    
-    # code if files are saved w/out timeperiod in the name
-    # for(j in 1:length(oceans)){
-    #     print(paste0('ocean', oceans[j]))
-    #     prestemp <- fread(cmd = paste0('gunzip -c temp/presmap_', oceans[j], '_rcp', rcps[i], '.csv.gz'), drop = 1)
-    #     biotemp <- fread(cmd = paste0('gunzip -c temp/biomassmap_', oceans[j], '_rcp', rcps[i], '.csv.gz'), drop = 1)
-    # 
-    #     # fix model #s for pres/abs projections (extras are in position 12 and 16)
-    #     # DELETE THIS AFTER UPDATING 5.0.1_process_species_proj.r to fix this problem in that script
-    #     prestemp <- prestemp[!(model %in% c(12, 16)), ]
-    #     prestemp[model > 12 & model < 16, model := model - 1L]
-    #     prestemp[model > 16, model := model - 2L]
-    #     
-    #     if(i == 1 & j == 1){
-    #         presmap <- prestemp
-    #         biomassmap <- biotemp
-    #     } else {
-    #         presmap <- rbind(presmap, prestemp)
-    #         biomassmap <- rbind(biomassmap, biotemp)
-    #     }
-    # }
         
     # code if files are saved by time period
     for(j in 1:length(oceans)){
@@ -123,7 +103,7 @@ rm(prestemp, biotemp)
 # use the thresholds calculated during model fitting from Morley et al. 2018 PLOS ONE
 poccurthresh <- fread('https://raw.githubusercontent.com/pinskylab/project_velocity/master/output/modeldiag_Nov2017_fitallreg_2017.csv', drop = 1)[, .(sppocean, thresh.kappa)]
 
-# definition of planning goals by region
+# definition of planning features by region
 sppfiles <- list.files(path = 'output/prioritizr_runs/', pattern = 'spp_*', full.names = TRUE)
 spps <- fread(sppfiles[1], drop = 1)
 spps[, region := gsub('/|output|prioritizr_runs|spp_|\\.csv', '', sppfiles[1])]
@@ -196,138 +176,6 @@ if(nrow(presmap) == nrow(presmapPac) + nrow(presmapAtl)){
 presmap[spp == 'theragra chalcogramma', spp := 'gadus chalcogrammus']
 biomassmap[spp == 'theragra chalcogramma', spp := 'gadus chalcogrammus']
 
-
-####################################################################################
-## Basic maps of solutions
-####################################################################################
-
-# plot map of selected grids (consplan #1 and #2)
-cexs = 0.5 # to adjust
-# quartz(width=5, height=18)
-pdf(width = 5, height = 18, file = 'figures/planmaps.pdf')
-par(mfrow = c(length(myregs),2), mai = c(0.5,0.5,0.3, 0.1), las = 1, mgp = c(2,1,0))
-for(i in 1:length(consplan1s)){
-    xlims <- range(consplan1s[[i]]$longrid)
-    ylims <- range(consplan1s[[i]]$latgrid)
-	j <- consplan1s[[i]]$solution_1_conservation == 1
-	plot(consplan1s[[i]]$longrid[j], consplan1s[[i]]$latgrid[j], pch=16, 
-	     col='red', cex=cexs, main='Historical only', xlab='', ylab='', xlim=xlims, ylim=ylims) # conservation
-	j <- consplan1s[[i]]$solution_1_fishery == 1
-	points(consplan1s[[i]]$longrid[j], consplan1s[[i]]$latgrid[j], pch=16, col='blue', cex=cexs) # fishery
-	j <- consplan1s[[i]]$solution_1_energy == 1
-	points(consplan1s[[i]]$longrid[j], consplan1s[[i]]$latgrid[j], pch=16, col='green', cex=cexs) # energy
-	j <- consplan1s[[i]]$solution_1_conservation == 0 & 
-	    consplan1s[[i]]$solution_1_fishery == 0 & consplan1s[[i]]$solution_1_energy == 0
-	points(consplan1s[[i]]$longrid[j], consplan1s[[i]]$latgrid[j], pch=16, col='grey', cex=cexs) # all others
-	
-	j <- consplan2s[[i]]$solution_1_conservation == 1
-	plot(consplan2s[[i]]$longrid[j], consplan2s[[i]]$latgrid[j], pch=16, col='red', cex=cexs, 
-	     main='Two periods', xlab='', ylab='', xlim=xlims, ylim=ylims) # conservation
-	j <- consplan2s[[i]]$solution_1_fishery == 1
-	points(consplan2s[[i]]$longrid[j], consplan2s[[i]]$latgrid[j], pch=16, col='blue', cex=cexs) # fishery
-	j <- consplan2s[[i]]$solution_1_energy == 1
-	points(consplan2s[[i]]$longrid[j], consplan2s[[i]]$latgrid[j], pch=16, col='green', cex=cexs) # energy
-	j <- consplan2s[[i]]$solution_1_conservation == 0 & consplan2s[[i]]$solution_1_fishery == 0 & 
-	    consplan2s[[i]]$solution_1_energy == 0
-	points(consplan2s[[i]]$longrid[j], consplan2s[[i]]$latgrid[j], pch=16, col='grey', cex=cexs) # all others
-}
-
-legend('topright', legend=c('Conservation', 'Fishery', 'Energy'), col=c('red', 'blue', 'green'), pch=rep(16,3), cex=0.7, title='Zones', bty='n')
-
-
-dev.off()
-
-
-############################
-## Basic stats on the plans
-############################
-# have to read in the plans above (but don't need to read in the species distributions)
-# climGrid <- read.csv('data/climGrid.csv', row.names=1) # for temperature climatology and depth
-
-# proportion of each region in each zone
-grids <- rbindlist(consplan1s)[, .(ngrid = .N), by = c('reg', 'zone')]
-grids[, total := sum(ngrid), by  = 'reg'][, prop := round(ngrid/total, 2)]
-setkey(grids, reg, zone)
-grids
-grids[ , .(meanprop = mean(prop), sdprop = sd(prop), seprop = sd(prop)/sqrt(.N)), by = 'zone']
-
-# latitudinal range by zone in each plan
-# do 2per plans span a wider latitudinal range?
-#	a <- lapply(pusplan1s, FUN=function(x) aggregate(list(latrng_histonly=x$lat), by=list(zone=x$zone), FUN=function(x) max(x)-min(x)))
-#	b <- lapply(pusplan2s, FUN=function(x) aggregate(list(latrng_2per=x$lat), by=list(zone=x$zone), FUN=function(x) max(x)-min(x)))
-	a <- lapply(consplan1s, FUN=function(x) x[ , .(latsd_hist = sd(latgrid), reg = unique(reg)), by = zone])
-	b <- lapply(consplan2s, FUN=function(x) x[ , .(latsd_2per = sd(latgrid), reg = unique(reg)), by = zone])
-	a <- do.call('rbind',a)
-	b <- do.call('rbind',b)
-	d <- merge(a,b)
-	d[d$zone==1,] # conservation
-	d[d$zone==2,] # fishing
-	d$diff <- d$latsd_2per - d$latsd_hist # positive if 2per spans a wider range
-	summary(d$diff)
-	wilcox.test(d$diff[d$zone==1])
-	wilcox.test(d$diff[d$zone==2])
-	wilcox.test(d$diff[d$zone==3])
-		# note: zone 1 conservation, 2 fishery, 3 energy
-	
-# temperature and depth range by zone in each plan
-	# add in climatology temperature
-# pusplan1swclim <- pusplan1s
-# pusplan2swclim <- pusplan2s
-# for(i in 1:length(pusplan1swclim)){
-# 	pusplan1swclim[[i]] <- merge(pusplan1s[[i]], climGrid[,c('lat', 'lon', 'bottemp.clim.int', 'surftemp.clim.int', 'depth')], all.x=TRUE)
-# 	pusplan2swclim[[i]] <- merge(pusplan2s[[i]], climGrid[,c('lat', 'lon', 'bottemp.clim.int', 'surftemp.clim.int', 'depth')], all.x=TRUE)
-# }
-# 
-# 	# do 2per plans span a wider surface temperature range?
-# #	a <- lapply(pusplan1swclim, FUN=function(x) aggregate(list(temprng_histonly=x$surftemp.clim.int), by=list(zone=x$zone), FUN=function(x) max(x, na.rm=TRUE)-min(x, na.rm=TRUE)))
-# #	b <- lapply(pusplan2swclim, FUN=function(x) aggregate(list(temprng_2per=x$surftemp.clim.int), by=list(zone=x$zone), FUN=function(x) max(x, na.rm=TRUE)-min(x, na.rm=TRUE)))
-# 	a <- lapply(pusplan1swclim, FUN=function(x) aggregate(list(tempsd_histonly=x$surftemp.clim.int), by=list(zone=x$zone), FUN=function(x) sd(x, na.rm=TRUE)))
-# 	b <- lapply(pusplan2swclim, FUN=function(x) aggregate(list(tempsd_2per=x$surftemp.clim.int), by=list(zone=x$zone), FUN=function(x) sd(x, na.rm=TRUE)))
-# 	a <- do.call('rbind',a)
-# 	b <- do.call('rbind',b)
-# 	a$reg <- gsub('.[[:digit:]]', '', row.names(a))
-# 	b$reg <- gsub('.[[:digit:]]', '', row.names(b))
-# 	d <- merge(a,b)
-# 	d[d$zone==2,] # conservation
-# 	d[d$zone==3,] # fishing
-# 	d$diff <- d$temprng_2per - d$temprng_histonly # positive if 2per spans a wider range
-# 	summary(d$diff)
-# 	wilcox.test(d$diff[d$zone==2])
-# 	wilcox.test(d$diff[d$zone==3])
-# 
-# 	# do 2per plans span a wider bottom temperature range?
-# #	a <- lapply(pusplan1swclim, FUN=function(x) aggregate(list(temprng_histonly=x$bottemp.clim.int), by=list(zone=x$zone), FUN=function(x) max(x, na.rm=TRUE)-min(x, na.rm=TRUE)))
-# #	b <- lapply(pusplan2swclim, FUN=function(x) aggregate(list(temprng_2per=x$bottemp.clim.int), by=list(zone=x$zone), FUN=function(x) max(x, na.rm=TRUE)-min(x, na.rm=TRUE)))
-# 	a <- lapply(pusplan1swclim, FUN=function(x) aggregate(list(tempsd_histonly=x$bottemp.clim.int), by=list(zone=x$zone), FUN=function(x) sd(x, na.rm=TRUE)))
-# 	b <- lapply(pusplan2swclim, FUN=function(x) aggregate(list(tempsd_2per=x$bottemp.clim.int), by=list(zone=x$zone), FUN=function(x) sd(x, na.rm=TRUE)))
-# 	a <- do.call('rbind',a)
-# 	b <- do.call('rbind',b)
-# 	a$reg <- gsub('.[[:digit:]]', '', row.names(a))
-# 	b$reg <- gsub('.[[:digit:]]', '', row.names(b))
-# 	d <- merge(a,b)
-# 	d[d$zone==2,] # conservation
-# 	d[d$zone==3,] # fishing
-# 	d$diff <- d$temprng_2per - d$temprng_histonly # positive if 2per spans a wider range
-# 	summary(d$diff)
-# 	wilcox.test(d$diff[d$zone==2])
-# 	wilcox.test(d$diff[d$zone==3])
-# 
-# 
-# 	# do 2per plans span a wider depth range?
-# 	a <- lapply(pusplan1swclim, FUN=function(x) aggregate(list(depthrng_histonly=x$depth), by=list(zone=x$zone), FUN=function(x) max(x, na.rm=TRUE)-min(x, na.rm=TRUE)))
-# 	b <- lapply(pusplan2swclim, FUN=function(x) aggregate(list(depthrng_2per=x$depth), by=list(zone=x$zone), FUN=function(x) max(x, na.rm=TRUE)-min(x, na.rm=TRUE)))
-# 	a <- do.call('rbind',a)
-# 	b <- do.call('rbind',b)
-# 	a$reg <- gsub('.[[:digit:]]', '', row.names(a))
-# 	b$reg <- gsub('.[[:digit:]]', '', row.names(b))
-# 	d <- merge(a,b)
-# 	d[d$zone==2,] # conservation
-# 	d[d$zone==3,] # fishing
-# 	d$diff <- d$temprng_2per - d$temprng_histonly # positive if 2per spans a wider range
-# 	summary(d$diff)
-# 	wilcox.test(d$diff[d$zone==2])
-# 	wilcox.test(d$diff[d$zone==3])
-# 
 	
 ######################################################################
 # Analyze plans against each climate model projection                #
@@ -355,7 +203,7 @@ for(i in 1:length(myregs)){
 	totalpres <- as.data.table(expand.grid(spp=spps[!grepl('fishery|energy', name) & region == myregs[i], spp], year_range = periods, model = modelsp, rcp=rcps)) # grid of all spp and time periods and models and rcps
 	if(totalpres[, .(lu(spp)*lu(year_range)*lu(model)*lu(rcp))] != nrow(totalpres)) stop(paste('i=', i, 'and rows in totalpres are not right'))
 	
-	totprescalcs <- presmap[region == myregs[i], .(total = sum(poccur > poccurthresh)), by = c("spp", "year_range", "model", "rcp")]
+	totprescalcs <- presmap[region == myregs[i], .(total = sum(poccur > thresh.kappa)), by = c("spp", "year_range", "model", "rcp")]
         dim(totprescalcs)
     
 	totalpres <- merge(totalpres, totprescalcs, all.x=TRUE) # make sure all spp and time periods and models and rcps represented
@@ -366,9 +214,9 @@ for(i in 1:length(myregs)){
 	temp1 <- merge(presmap[region == myregs[i], ], consplan1s[[i]][zone == 1, .(latgrid, longrid, zone)], by=c('latgrid', 'longrid')) # only keep the conserved zones for goals related to climate
 		dim(temp1) # a data.table
 	temp2 <- merge(presmap[region == myregs[i], ], consplan2s[[i]][zone == 1, .(latgrid, longrid, zone)], by=c('latgrid', 'longrid'))
-	presbyzone1 <- temp1[, .(zonetotal = sum(poccur > poccurthresh)), by = c('spp', 'year_range', 'zone', 'model', 'rcp')]
+	presbyzone1 <- temp1[, .(zonetotal = sum(poccur > thresh.kappa)), by = c('spp', 'year_range', 'zone', 'model', 'rcp')]
 		dim(presbyzone1) # 19392 (ebs)
-	presbyzone2 <- temp2[, .(zonetotal = sum(poccur > poccurthresh)), by = c('spp', 'year_range', 'zone', 'model', 'rcp')]
+	presbyzone2 <- temp2[, .(zonetotal = sum(poccur > thresh.kappa)), by = c('spp', 'year_range', 'zone', 'model', 'rcp')]
 		dim(presbyzone2)
 
 	# add totals for pres across all zones
@@ -495,147 +343,5 @@ write.csv(goalsmetbymod1out, file = paste0('output/goalsmetbymod_', runname1out,
 write.csv(goalsmetbymod2out, file = paste0('output/goalsmetbymod_', runname2out, '.csv'))
 
 
-######################################################################
-# Compare and plot the planning approaches against all climate models
-######################################################################
-goalsmetbymod1out <- fread(paste0('output/goalsmetbymod_', runname1out, '.csv'), drop = 1)
-goalsmetbymod2out <- fread(paste0('output/goalsmetbymod_', runname2out, '.csv'), drop = 1)
-
-
-mods <- sort(unique(goalsmetbymod1out$model))
-rcps <- sort(unique(goalsmetbymod1out$rcp))
-regnames<-c('E. Bering Sea', 'Gulf of AK', 'British Columbia', 'West Coast US', 'Gulf of Mexico', 'Southeast US', 'Northeast US', 'Maritimes', 'Newfoundland')
-
-# add plan identifier
-goalsmetbymod1out$plan <- 'histonly'
-goalsmetbymod2out$plan <- '2per'
-
-# combine
-goalsmetbymod <- rbind(goalsmetbymod1out, goalsmetbymod2out)
-
-
-# compare goals met across plans and time periods
-goalmetbymodag <- with(rbind(goalsmetbymod1out, goalsmetbymod2out), 
-                       aggregate(list(pmet = pmet), by = list(plan = plan, model = model, rcp = rcp, period = year_range), FUN = mean))
-with(goalmetbymodag, aggregate(list(ave_goals = pmet), by = list(plan = plan, period = period), FUN = mean))
-with(goalmetbymodag, aggregate(list(ave_goals = pmet), by = list(plan = plan, period = period), FUN = function(x) sd(x)/length(x)))
-with(goalmetbymodag, aggregate(list(ave_goals = pmet), by = list(plan = plan, period = period), FUN = length))
-
-
-# test whether histonly meets significantly fewer goals than 2per
-	all(goalsmetbymod1out$model == goalsmetbymod2out$model & goalsmetbymod1out$period == goalsmetbymod2out$period & goalsmetbymod1out$rcp == goalsmetbymod2out$rcp) # same order?
-
-	# simple t-test on number met (pseudoreplicates within regions)
-	t.test(goalsmetbymod1out$nmet[goalsmetbymod1out$period=='2081-2100'], goalsmetbymod2out$nmet[goalsmetbymod2out$period=='2081-2100'])
-
-	# simple t-test on proportions (should use logistic) (also pseudoreplicates within regions)
-	t.test(goalsmetbymod1out$pmet[goalsmetbymod1out$period=='2081-2100'], goalsmetbymod2out$pmet[goalsmetbymod2out$period=='2081-2100'], paired=FALSE)
-
-	# GLMM model (since proportion data) on 2006-2020
-	require(lme4)
-	require(car)
-	mod <- glmer(cbind(nmet, nmet/pmet - nmet) ~ plan + (1|region/rcp/model), data=goalsmetbymod, family='binomial', subset=goalsmetbymod$period=='2006-2020')
-	summary(mod)
-	Anova(mod)
-
-	# GLMM model (since proportion data) on 2041-2060
-	require(lme4)
-	require(car)
-	goalsmetbymod <- rbind(goalsmetbymod1out, goalsmetbymod2out)
-	mod <- glmer(cbind(nmet, nmet/pmet - nmet) ~ plan + (1|region/rcp/model), data=goalsmetbymod, family='binomial', subset=goalsmetbymod$period=='2041-2060')
-	summary(mod)
-	Anova(mod)
-
-	# GLMM model (since proportion data) on 2081-2100
-	goalsmetbymod <- rbind(goalsmetbymod1out, goalsmetbymod2out)
-	mod <- glmer(cbind(nmet, nmet/pmet - nmet) ~ plan + (1|region/rcp/model), data=goalsmetbymod, family='binomial', subset=goalsmetbymod$period=='2081-2100')
-	summary(mod)
-	Anova(mod)
-
-	# GLMM model (since proportion data) across all time periods
-	goalsmetbymod <- rbind(goalsmetbymod1out, goalsmetbymod2out)
-	mod <- glmer(cbind(nmet, nmet/pmet - nmet) ~ plan + (1|region/rcp/model/period), data=goalsmetbymod, family='binomial')
-	summary(mod)
-	Anova(mod)
-
-
-# plot %goals met (solution #1 and #2) for non-planning models
-	# thesemods <- mods[gcminds]; type <- 'testing' # for models not used in planning
-	thesemods <- mods[-gcminds]; type <- 'training' # for models used in planning
-	
-	colmat <- t(col2rgb(brewer.pal(4, 'Paired')))
-#	colmat <- t(col2rgb(brewer.pal(4, 'Dark2')))
-	cols <- rgb(red=colmat[,1], green=colmat[,2], blue=colmat[,3], alpha=c(255,255,255,255), maxColorValue=255)
-	if(type == 'testing') outfile <- paste('figures/prioritizr_allregs_goalsmetbymod_', runname1out, '&', runname2out, '.pdf', sep='')
-	if(type == 'training') outfile <- paste('temp_figures/prioritizr_allregs_goalsmetbymod_', runname1out, '&', runname2out, '_training.pdf', sep='')
-	    outfile
-	ylims <- c(0,1)
-
-	# quartz(width=6, height=6)
-	pdf(width=6, height=6, file=outfile)
-	par(mfrow=c(3,3), mai=c(0.3, 0.35, 0.2, 0.05), omi=c(0.2,0.2,0,0), cex.main=0.8)
-
-	for(i in 1:length(myregs)){
-		# each model/rcp for hist only plans
-	    inds <- goalsmetbymod1out$model == thesemods[1] & goalsmetbymod1out$rcp == rcps[1] & goalsmetbymod1out$region==myregs[i]
-		plot(goalsmetbymod1out$mid[inds], goalsmetbymod1out$pmet[inds], xlab='', ylab='', ylim=ylims, type='l', pch=16, las=1, col=cols[1], main=regnames[i])
-		for(k in 1:length(thesemods)){
-			for(j in 1:length(rcps)){
-				if(!(k==1 & j==1)){
-					inds <- goalsmetbymod1out$model == thesemods[k] & goalsmetbymod1out$rcp == rcps[j] & goalsmetbymod1out$region==myregs[i]
-					points(goalsmetbymod1out$mid[inds], goalsmetbymod1out$pmet[inds], type='l', pch=16, col=cols[1])	
-				}
-			}
-		}
-		
-        # each model/rcp for 2per plans
-		for(k in 1:length(thesemods)){
-			for(j in 1:length(rcps)){
-				inds <- goalsmetbymod2out$model == thesemods[k] & goalsmetbymod2out$rcp == rcps[j] & goalsmetbymod2out$region==myregs[i]
-				points(goalsmetbymod2out$mid[inds], goalsmetbymod2out$pmet[inds], type='l', pch=16, col=cols[3])
-			}	
-		}
-
-		# average across models/rcps for hist and 2per plans
-		inds <- goalsmetbymod1out$region==myregs[i] & goalsmetbymod1out$model %in% thesemods
-		ensmean <- aggregate(list(nmet=goalsmetbymod1out$nmet[inds], pmet=goalsmetbymod1out$pmet[inds]), by=list(mid=goalsmetbymod1out$mid[inds]), FUN=mean)
-		lines(ensmean$mid, ensmean$pmet, col=cols[2], lwd=3)
-		
-		inds <- goalsmetbymod2out$region==myregs[i] & goalsmetbymod2out$model %in% thesemods
-		ensmean2 <- aggregate(list(nmet=goalsmetbymod2out$nmet[inds], pmet=goalsmetbymod2out$pmet[inds]), by=list(mid=goalsmetbymod2out$mid[inds]), FUN=mean)
-		lines(ensmean2$mid, ensmean2$pmet, col=cols[4], lwd=3)
-		
-		# legend in plot 1
-		if(i == 1) legend('bottomright', legend = c('hist', '2per'), col = cols[c(2,4)], lwd = 1)
-	}
-	mtext(side=1,text='Year',line=0.5, outer=TRUE)
-	mtext(side=2,text='Fraction goals met',line=0.3, outer=TRUE)
-	
-	dev.off()
-	
-	
-## probability of < 80% goals met by region, plan, and time period
-	# calcs
-	u80func <- function(x) return(sum(x<0.8)/length(x))	
-	u80 <- with(goalsmetbymod, aggregate(list(u80=pmet), by=list(region=region, period=year_range, plan=plan), FUN=u80func))
-
-
-	# examine
-	u80[u80$period=='2041-2060',] # middle of century, by region
-	u80[u80$period=='2081-2100',] # end of century, by region
-	
-	with(u80[u80$period=='2007-2020',], aggregate(list(meanu80=u80), by=list(plan=plan), FUN=mean)) # means by plan (across regions), start
-	with(u80[u80$period=='2041-2060',], aggregate(list(meanu80=u80), by=list(plan=plan), FUN=mean)) # means by plan (across regions), middle of century
-	with(u80[u80$period=='2081-2100',], aggregate(list(meanu80=u80), by=list(plan=plan), FUN=mean)) # means by plan (across regions), end of century
-
-	a<-aggregate(list(max_u80=u80$u80), by=list(region=u80$region, plan=u80$plan), FUN=max) # max probability of being under 80% of goals met (look across time periods)
-		mean(a$max_u80[a$plan=='2per'])
-		mean(a$max_u80[a$plan=='histonly'])
-	
-	# plot
-	ggplot(u80, aes(x = period, y = u80, group = plan, color = plan)) +
-	    geom_line() + 
-	    geom_point() +
-	    facet_wrap(~ region, ncol = 3)
 	    
 	
